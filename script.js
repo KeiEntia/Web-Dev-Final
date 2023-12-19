324cb3c90bc08c7ad4815e7ab61b2d7fe6e154a2
KeiEntia <154311511+KeiEntia@users.noreply.github.com>
2023-12-19 15:41:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+  var backToTopButton = document.querySelector('.back-to-top');
+  if (window.scrollY > 200) {
+    backToTopButton.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+});
+
+
